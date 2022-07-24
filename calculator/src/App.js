0b0c2814c7ac596eb5ca197644e2ee11c832b75b
Vanika Patel <vanikadpatel@gmail.com>
2022-07-24 18:51:24 +0100

    modified component files, index.css, index.js and App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from './components/Wrapper';
+import Screen from './components/Screen';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "X"],
+  [1, 2, 3, "X"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  return(
+    <Wrapper>
+      <screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
